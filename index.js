cc7d06663f43881fab776e315794fdf5eb2ca949
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-12 10:50:44 -0400

    commit tree db70ce93ff0a659fea11ac36ec343e9e48f057dd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching (driversArray, driversName){
+    return driversArray.filter(function (name) {
+        return name.toLowerCase().indexOf(driversName.toLowerCase()) !== -1;
+    })
+}
+
+function fuzzyMatch(driversArray, driverFirstLetters){
+    return driversArray.filter(function (letters) {
+         return letters.toLowerCase().substring(0, 2) === driverFirstLetters.toLowerCase();
+    })
+}
+
+function matchName(driversArray, driversName){
+    return driversArray.filter(function (x) {
+        return (x.name.toLowerCase()) === driversName.toLowerCase();
+   })
+}
